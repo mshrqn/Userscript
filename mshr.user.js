78296d80e614f5140813719032d999fca0efb7b2
Tuan Khang <trumdameacc333@gmail.com>
2026-03-15 11:44:16 +0700

    Add files via upload

---
+++ mshr.user.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         qninlove
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Bypass tools easy
+// @author       mshr
+//
+// @match        https://*.work.ink/*
+// @match        http://*.work.ink/*
+// @match        https://work.ink/*
+// @match        http://work.ink/*
+//
+// @match        https://*.lootdest.org/*
+// @match        http://*.lootdest.org/*
+// @match        https://lootdest.org/*
+// @match        http://lootdest.org/*
+//
+// @match        https://*.loot-link.com/*
+// @match        http://*.loot-link.com/*
+// @match        https://loot-link.com/*
+// @match        http://loot-link.com/*
+//
+// @match        https://*.loot-links.com/*
+// @match        http://*.loot-links.com/*
+// @match        https://loot-links.com/*
+// @match        http://loot-links.com/*
+//
+// @match        https://linkvertise.com/*
+//
+// @match        https://cuttlinks.com/*
+//
+// @match        https://shrtslug.biz/*
+// @match        https://biovetro.net/*
+// @match        https://technons.com/*
+// @match        https://yrtourguide.com/*
+// @match        https://tournguide.com/*
+//
+// @match       https://rekonise.com/*
+//
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceText
+// @grant        GM_registerMenuCommand
+// @grant        unsafeWindow
+//
+// @connect      localhost
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @resource     NOTYF_CSS https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css
+// @require      https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js
+// @require      https://raw.githubusercontent.com/mshrqn/Userscript/refs/heads/main/main.js
+// @require      https://raw.githubusercontent.com/mshrqn/Userscript/refs/heads/main/updatelog.js
+// @downloadURL  https://raw.githubusercontent.com/mshrqn/Userscript/refs/heads/main/mshr.user.js
+// @updateURL    https://raw.githubusercontent.com/mshrqn/Userscript/refs/heads/main/mshr.user.js
+//
+// @run-at       document-start
+// ==/UserScript==
